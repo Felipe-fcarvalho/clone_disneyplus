214f119655d04ebe06d908cd8e0256ddc3d4c06a
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-06-09 16:56:29 -0300

    Configura Gulp com Sass e imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+import gulpSass from 'gulp-sass';
+import * as sassCompiler from 'sass';
+
+const sass = gulpSass(sassCompiler);
+
+function images() {
+    return gulp.src('./src/images/**/*', { encoding: false })
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+}
 
 function styles() {
-    return gulp.src('./src/styles/*.scss') //recupera arquivos
-        .pipe(sass({ style: 'compressed' }))
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+export function watch() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
 }
+
+export default gulp.parallel(styles, images);
