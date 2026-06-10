e620f6cc17d57275d16cdcc452be8030a3aa1a4e
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-06-10 19:56:41 -0300

    added: FAQ

--- src/main.js
+++ src/main.js
@@ -1,17 +1,31 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
-            const tabAlvo = botao.target.dataset.tabButton
-            const aba = document.querySelector(`[data-tab-id=${tabAlvo}]`)
+            const tabAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id="${tabAlvo}"]`);
             hideAllTabs();
             aba.classList.add('shows__list--is-active');
             removeActiveButton();
             botao.target.classList.add('shows__tabs__button--is-active');
         }) 
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', toggleFaq);
+    }
 })
+
+function toggleFaq(elemento) {
+    const  classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.closest('.faq__questions__item');
+
+    if (elementoPai) {
+        elementoPai.classList.toggle(classe);
+    }
+}
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
