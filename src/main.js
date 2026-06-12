09998a76c46c6212432ff40de6429d283c3e69f6
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-06-12 16:00:50 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < heroHeight) {
+            hiddenHeaderElements();
+        } else {
+            showHeaderElements();
+        }
+    })
     
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const tabAlvo = botao.target.dataset.tabButton;
@@ -13,10 +27,21 @@ document.addEventListener('DOMContentLoaded', function(){
         }) 
     }
 
+    // Seção FAQ, accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', toggleFaq);
     }
 })
+
+function hiddenHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function toggleFaq(elemento) {
     const  classe = 'faq__questions__item--is-open';
