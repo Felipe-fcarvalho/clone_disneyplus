8084fef0075e00dc2681c1e6a7ec49dcbab8db79
felipe-fcarvalho <felipefcarvalho7@gmail.com>
2026-06-12 16:20:19 -0300

    configurando o terser

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
 import gulpSass from 'gulp-sass';
 import * as sassCompiler from 'sass';
+import terser from 'gulp-terser';
 
 const sass = gulpSass(sassCompiler);
 
@@ -13,12 +14,19 @@ function images() {
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass({ style: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
 
-export function watch() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(terser())
+        .pipe(gulp.dest('./dist/js'));
 }
 
-export default gulp.parallel(styles, images);
+export function watch() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+}
+
+export default gulp.parallel(styles, images, scripts);
